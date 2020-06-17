fb12d4548677189da41f55eb1f18b13526499473
agusprogrammer <3dagustingonzalez1@gmail.com>
2020-06-17 18:17:39 +0200

    Commit de prueba 2

--- Angular.api.redSocial2/Servicios/UsuarioServicio.cs
+++ Angular.api.redSocial2/Servicios/UsuarioServicio.cs
@@ -147,7 +147,6 @@ namespace Angular.api.redSocial2.Servicios
             string resultado = "";
             
 
-
             string query = "";
             if (usu.idUsu <= 0 | usu.idUsu == null)
             {
